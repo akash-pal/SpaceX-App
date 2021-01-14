801ac12017800e2ebf5813a477c32474c55125bc
Akash Pal <akashpa93@gmail.com>
2021-01-14 17:49:41 +0530

    dashboard page added

---
+++ pages/dashboard.js
@@ -0,0 +1,21 @@
+function Dashboard({ data }) {
+    return (
+        <div>
+            {data && data.map(item => (
+                <div key={item?.flight_number}>
+                    <h1>{item?.mission_name}</h1>
+                </div>
+            )
+            )}
+        </div>
+    )
+}
+
+export async function getServerSideProps() {
+    const URL = 'https://api.spacexdata.com/v3/launches?limit=10';
+    const res = await fetch(URL);
+    const data = await res.json();
+    return { props: { data } }
+}
+
+export default Dashboard;
