b8fb91ceaa8e9ff2580e79f48a54cc0ceea9d3e8
Akash Pal <akashpa93@gmail.com>
2021-01-17 01:21:27 +0530

    Removed dashboard and moved to index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,51 @@
-import Dashboard from './dashboard';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import FiltersView from "../components/FiltersView";
 
-export default function Home() {
+const baseURL = 'https://api.spacexdata.com/v3/launches?limit=10';
+
+function makeQueryParams(query) {
+  let url = baseURL;
+  if (query.launch) {
+    url += `&launch_success=${query.launch}`
+  }
+  if (query.landing) {
+    url += `&land_success=${query.landing}`
+  }
+  if (query.selectedYear) {
+    url += `&launch_year=${query.selectedYear}`;
+  }
+  return url;
+}
+
+function Home({ response }) {
+  const { query } = useRouter();
+  const [serverQuery] = useState(query);
+  const { data } = useSWR(makeQueryParams(query), {
+    dedupingInterval: 6000,
+    initialData: (JSON.stringify(serverQuery) === JSON.stringify(query)) ? response : null,
+    revalidateOnFocus: false 
+  });
   return (
-    <Dashboard />
+    <div>
+      <h1>SpaceX Launch Programs</h1>
+      <FiltersView />
+      {data && data.map((item) => (
+        <div key={item?.flight_number}>
+          <h1>{item?.mission_name}</h1>
+        </div>
+      ))}
+    </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const URL = makeQueryParams(query);
+  const res = await fetch(URL);
+  const response = await res.json();
+  return { props: { response } };
+}
+
+export default Home;
